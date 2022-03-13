65485ac292ccc02ad3935b01a1949215727e39e8
Burak Bal <burak.bal@ceiba-healthcare.com>
2022-03-13 16:52:09 +0300

    config-overrides.js cleaning

--- config-overrides.js
+++ config-overrides.js
@@ -1,68 +1,6 @@
 const { override, addWebpackModuleRule } = require('customize-cra');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
-const addSassOptions =
-    (loaderOptions = {}, customCssModules = {}) =>
-    (config) => {
-        const webpackEnv = process.env.NODE_ENV;
-        const isEnvDevelopment = webpackEnv === 'development';
-        const isEnvProduction = webpackEnv === 'production';
-        const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
-
-        const getStyleLoaders = (cssOptions, preProcessor) => {
-            const loaders = [
-                isEnvDevelopment && require.resolve('style-loader'),
-                // isEnvProduction && {
-                //     loader: MiniCssExtractPlugin.loader,
-                //     options: shouldUseRelativeAssetPaths ? { publicPath: "../../" } : {}
-                // },
-                {
-                    loader: require.resolve('css-loader'),
-                    options: cssOptions
-                },
-                {
-                    loader: require.resolve('postcss-loader'),
-                    options: {
-                        ident: 'postcss',
-                        plugins: () => [
-                            require('postcss-flexbugs-fixes'),
-                            require('postcss-preset-env')({
-                                autoprefixer: {
-                                    flexbox: 'no-2009'
-                                },
-                                stage: 3
-                            }),
-                            postcssNormalize()
-                        ],
-                        sourceMap: isEnvProduction && shouldUseSourceMap
-                    }
-                }
-            ].filter(Boolean);
-            if (preProcessor) {
-                loaders.push(
-                    {
-                        loader: require.resolve('resolve-url-loader'),
-                        options: {
-                            sourceMap: isEnvProduction && shouldUseSourceMap
-                        }
-                    },
-                    {
-                        loader: require.resolve(preProcessor),
-                        // not the same as react-scripts
-                        options: Object.assign(
-                            {
-                                sourceMap: true
-                            },
-                            loaderOptions
-                        )
-                    }
-                );
-            }
-            return loaders;
-        };
-        return config;
-    };
-
 module.exports = override(
     addWebpackModuleRule({
         test: /\.module\.s[ac]ss$/i,
