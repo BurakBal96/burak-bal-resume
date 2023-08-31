15021edb1d2d1bdde9928c5553abb7919e38d038
BurakBal96 <burakbal1296@gmail.com>
2023-08-31 17:30:44 +0300

    FrontendSkills structure change

--- src/Pages/Resume/Sections/FrontendSkills.js
+++ src/Pages/Resume/Sections/FrontendSkills.js
@@ -2,25 +2,14 @@ import React from 'react';
 import { Section, Title, Entry, List } from '../Section';
 
 const list = [
-    'React',
-    'JavaScript TypeScript',
-    'HTML/CSS',
-    'NodeJS',
-    'MobX - State Management',
-    'Cypress',
-    'Jest',
-    'Mocha',
-    'Selenium',
-
-    'AWS',
-    'Git',
-    'Jenkins',
-    'Storybook',
-    'Docker',
-    'npm',
-    "CI/CD",
-    "SonarQube"
-];
+    ['React', 'JavaScript TypeScript', 'HTML/CSS'],
+    ['Sass/Scss', 'MobX - State Management', 'Figma'],
+    ['NodeJS', 'Jest', 'Cypress'],
+    ['Mocha', 'Selenium', 'AWS'],
+    ['Git', 'Jenkins', 'Storybook'],
+    ['Docker', 'npm', 'CI/CD'],
+    ['SonarQube']
+].flat();
 
 export const FrontendSkills = ({ ...props }) => {
     return (
